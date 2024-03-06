846ecb7c2c139733f34bcb098a951be003cf225d
wiktorsk8 <wikto4k@gmail.com>
2024-03-06 22:25:03 +0100

    Added database config for pipeline tests

--- project_finder/settings.py
+++ project_finder/settings.py
@@ -81,14 +81,12 @@ WSGI_APPLICATION = 'project_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'pgsql',
-        # 'USER': 'postgres',
-        # 'PASSWORD': '123456789',
-        # 'HOST': 'db',
-        # 'PORT': '5432',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pgsql',
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
