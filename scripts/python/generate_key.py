3b17f6081acac499ab77b4d035b7e04fccb42c0d
lukasz <lukol12321@gmail.com>
2024-04-03 12:11:31 +0200

    Script fix

--- scripts/python/generate_key.py
+++ scripts/python/generate_key.py
@@ -1,13 +1,14 @@
+import os.path
+
 from django.core.management.utils import get_random_secret_key
-import os
+from pathlib import Path
 
-
-PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
+PROJECT_PATH = Path(__file__).resolve().parent.parent.parent
 
 
 def generate():
     try:
-        with open(PROJECT_PATH + '/.env', 'a') as file:
+        with open(os.path.join(PROJECT_PATH, '.env'), 'a') as file:
             print('\nSECRET_KEY=' + get_random_secret_key(), file=file)
     except FileNotFoundError:
         print('.env file not found.')
